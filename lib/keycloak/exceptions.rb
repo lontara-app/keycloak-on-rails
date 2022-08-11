d4b037d0d30afdbd605f95f62e5f1e5cca6da7b0
GhaniyKie <abdulhakimghaniy@outlook.co.id>
2022-08-11 10:58:05 +0700

    debug

--- lib/keycloak/exceptions.rb
+++ lib/keycloak/exceptions.rb
@@ -4,5 +4,10 @@ module Keycloak
     class ProcCookieTokenNotDefined < KeycloakException; end
     class ProcExternalAttributesNotDefined < KeycloakException; end
     class InstallationFileNotFound < KeycloakException; end
-    class MethodNotSupported < KeycloakException; end
+    class MethodNotSupported < KeycloakException
+        def initialize(message = '', exception_type = '')
+            @exception_type = exception_type
+            super(message)
+        end
+    end
 end
