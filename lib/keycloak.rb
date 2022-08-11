f75bf1dd59fbc86700f731ef1cabba3774bac334
GhaniyKie <abdulhakimghaniy@outlook.co.id>
2022-08-11 13:51:33 +0700

    debug

--- lib/keycloak.rb
+++ lib/keycloak.rb
@@ -363,7 +363,7 @@ module Keycloak
       return { message: 'User not logged in or Token not provided' } if token.blank? && access_token.blank?
 
       access_token = token['access_token'] if access_token.empty?
-      print token[:'access_token']
+      print token
       JWT.decode access_token, @public_key, true, { algorithm: 'RS256' }
     end
 
